77e7d73c829179696d136d0a678d78d3ab3d541c
Abimael Santos <abimael@MacBook-Air-de-Abimael.local>
2025-01-31 21:54:06 -0300

    first commit

---
+++ hello-world-java/src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello, World!");
+        while (true) {
+            Thread.sleep(1000);
+        }
+    }
+}
